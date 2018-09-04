0298d92949402f1f5ec1e5a462ffb51291fa296b
Ryszard.Sinius <ryszard.sinius@tieto.com>
2018-09-04 06:46:15 -0700

    Structures sending modified

--- clientLTE/random_access.c
+++ clientLTE/random_access.c
@@ -16,8 +16,25 @@ int send_prach_preamble(int sockfd, s_message* message, void (*ra_rnti_generator
     //Then we need to set message type to RA_RNTI
     message->message_type = RA_RNTI;
     //Send struct
-    if(-1 == write(sockfd, &message, sizeof(message)))
+    if(-1 == write(sockfd, (s_message*)message, sizeof(message)))
         return -1;
 
+    return 0;
+}
+
+// This function receives response from eNodeB
+int receive_prach_response(int socketfd, s_message* message)
+{
+    if(-1 == read(socketfd, (s_message*)message, sizeof(*message)))
+        return -1;
+    return 0;
+}
+
+int main()
+{
+    s_message message;
+    message.message_type = RA_RNTI;
+    message.message_value.message_preamble.ra_rnti = 44;
+    send_prach_preamble(44, &message, generate_ra_rnti);
     return 0;
 }
