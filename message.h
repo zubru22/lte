148cee6e9afabec03752bcc5c4674e2cc3d3c7f0
Michal Zuber <michal.zuber@tieto.com>
2018-09-04 01:36:21 -0700

    Add union to message

--- message.h
+++ message.h
@@ -1,4 +1,4 @@
-#include <types.h>
+#include <sys/types.h>
 
 typedef struct prach_preamble {
     int16_t ra_rnti;
@@ -11,6 +11,8 @@ typedef enum type_of_message {RA_RNTI, C_RNTI} e_message_type;
 
 struct message {
     e_message_type message_type;
-    union {
+    union message_value {
+        preamble message_preamble;
+        response message_response;
     };
 };
