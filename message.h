d469ac723a6bc34a1dde91e64fef96951d2574d3
Michal Zuber <michal.zuber@tieto.com>
2018-09-04 03:09:21 -0700

    Add RAPID to prach response structure

--- message.h
+++ message.h
@@ -3,10 +3,10 @@
 #include <sys/types.h>
 
 typedef struct prach_preamble {
-    int16_t ra_rnti;
+    int16_t ra_rnti; // 16bit integer sent to server - first 2 bits are PreambleIndex
 } preamble;
 typedef struct prach_response {
-
+    int8_t rapid; // Get 2 first bits from ra_rnti and send it back to client
 } response;
 
 typedef enum type_of_message {RA_RNTI, C_RNTI} e_message_type;
