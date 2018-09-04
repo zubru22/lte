94aeb96eedad9018f26ad747638ec087b3e4f981
Michal Zuber <michal.zuber@tieto.com>
2018-09-04 01:00:21 -0700

    Include types.h and create first structures

--- message.h
+++ message.h
@@ -1,11 +1,16 @@
-struct RA_RNTI{
+#include <types.h>
 
-};
-struct C_RNTI{
-};
+typedef struct prach_preamble {
+    int16_t ra_rnti;
+} preamble;
+typedef struct prach_response {
 
-struct message{
-    enum type_of_message{RA_RNTI, C_RNTI};
+} response;
+
+typedef enum type_of_message {RA_RNTI, C_RNTI} e_message_type;
+
+struct message {
+    e_message_type message_type;
     union {
     };
 };
