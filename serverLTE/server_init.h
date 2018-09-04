57c593b952f52bcc322b4028ad421fd7ef7429d6
Igor-Lisowski <Igor-Lisowski@gitlab.com>
2018-09-04 01:04:34 -0700

    add clients variable storing clients, server_t struct storing socket of server, and empty init_server function, which will init the server

--- serverLTE/server_init.h
+++ serverLTE/server_init.h
@@ -2,6 +2,21 @@
 const int MAX_EVENTS = 100;
 const int MAX_CLIENTS = 10;
 
+client **clients;
+
 typedef struct {
   int socket;
-} client;
+} client_t;
+
+typedef struct {
+  int socket;
+} server_t;
+
+
+
+
+void init_server() {
+
+}
+
+void
