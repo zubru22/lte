c339045f05e94cc396ba56ca8c693d34b6750f82
Michal Zuber <michal.zuber@tieto.com>
2018-09-05 06:32:57 -0700

    Change integers to unsigned integers

--- message.h
+++ message.h
@@ -15,8 +15,8 @@ typedef enum type_of_message {RA_RNTI, C_RNTI, RRC_REQ} e_message_type;
 typedef enum establishment_cause {EMERGENCY, HIGH_PRIORITY_ACCESS, MT_ACCESS, MO_SIGNALING, MO_DATA} e_ec;
 
 typedef struct stmsi {
-    int8_t mme_code;
-    int32_t mtmsi;
+    u_int8_t mme_code;
+    u_int32_t mtmsi;
 } s_stmsi;
 
 typedef struct rrc_req {
