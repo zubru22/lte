77c9145c5a698c425aac3d5cdf71750a2c7957f3
Michal Elszkowski <michal.elszkowski@tieto.com>
2018-09-05 05:26:42 -0700

    add timestamp to CRNTI response

--- message.h
+++ message.h
@@ -1,12 +1,14 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 #include <sys/types.h>
+#include <time.h>
 
 typedef struct prach_preamble {
     int16_t ra_rnti; // 16bit integer sent to server - first 2 bits are PreambleIndex
 } preamble;
 typedef struct prach_response {
     int8_t rapid; // Get 2 first bits from ra_rnti and send it back to client
+    time_t unix_epoch_timestamp;
 } response;
 
 typedef enum type_of_message {RA_RNTI, C_RNTI} e_message_type;
