f36a524c64c8bb0a8545de1c31d0807ad826b4a7
Michal Zuber <michal.zuber@tieto.com>
2018-09-04 23:52:51 -0700

    Fix warnings about implicit function declarations caused by missing header files

--- clientLTE/init_connection.h
+++ clientLTE/init_connection.h
@@ -1,8 +1,9 @@
-    #include <stdio.h>
+#ifndef INIT_CONNECTION_H
+#define INIT_CONNECTION_H
     #include <string.h>
-    #include <sys/socket.h>
-    #include <arpa/inet.h>
     #include <netdb.h>
     #include <sys/types.h>
-    
+    #include "random_access.h"
+
     int init_connection(int* socket_fd, struct sockaddr_in* server);
+#endif
