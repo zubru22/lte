b3a86b1ad37df049fb03e3643664f36c9f656d21
Ryszard.Sinius <ryszard.sinius@tieto.com>
2018-09-05 07:09:57 -0700

    ue_identity generator function added

--- clientLTE/rrc.c
+++ clientLTE/rrc.c
@@ -1,4 +1,12 @@
 /* This functionality is about sending RRC Connection Request to eNodeB, 
 * wait for RCC Connection Setup from RRC and finally send back
 * the RRC Connection Complete message */
+#include "rrc.h"
+
+// This function generates ue_identity random key
+void generate_ue_identity(s_stmsi* ue_identity)
+{
+    ue_identity->mme_code = rand() % (int)pow(2, 8);
+    ue_identity->mtmsi = rand() % (int)pow(2, 32);
+}
 
--- clientLTE/rrc.h
+++ clientLTE/rrc.h
@@ -1,4 +1,10 @@
+#ifndef RRC_H
+#define RRC_H
+
 #ifndef MESSAGE_H
 #include "../message.h"
 #endif
 
+void generate_ue_identity(s_stmsi*);
+
+#endif
