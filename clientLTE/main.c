2db7f4f3511e8a7e761702a2e00d44b06beb4631
marcin <marcin.nikolajczyk@tieto.com>
2018-09-04 23:59:00 -0700

    Fixed spelling mistake

--- clientLTE/main.c
+++ clientLTE/main.c
@@ -5,16 +5,14 @@
 int main(int argc, char* argv[])
 {
     if(argc < 2){
-        printf("You need to pass porn number as an argument!\n");
+        printf("You need to pass port number as an argument!\n");
         return 0;
     }
     int port_number = atoi(argv[1]);
     int socket_fd;
     struct sockaddr_in server;
     s_message message;
-    srand(time(NULL));
-    printf("%i", argc);
-    
+    srand(time(NULL)); 
 
     //init_connection returns 0 on error, else function returns 1
     if (init_connection(&socket_fd, &server, port_number)) {
