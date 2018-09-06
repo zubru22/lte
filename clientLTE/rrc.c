7d75b46dc03b0c2744b629efc1e35157895e8e4b
Michal Zuber <michal.zuber@tieto.com>
2018-09-06 02:27:04 -0700

    Create rrc_complete structure and function to send plmn_id

--- clientLTE/rrc.c
+++ clientLTE/rrc.c
@@ -1,5 +1,5 @@
 /* This functionality is about sending RRC Connection Request to eNodeB, 
-* wait for RCC Connection Setup from RRC and finally send back
+* waiting for RCC Connection Setup from RRC and finally sending back
 * the RRC Connection Complete message */
 #include "rrc.h"
 
@@ -14,6 +14,17 @@ int send_rrc_connection_request(int socketfd, s_message* message, void (*generat
     message->message_type = rrc_request;
     generate_ue_identity_func(&message->message_value.message_request.ue_identity);
     message->message_value.message_request.establishment_cause = MO_SIGNALING;
+
+    if(-1 == write(socketfd, (s_message*)message, sizeof(*message)))
+        return -1;
+    return 0;
+}
+
+// This function sends rrc setup complete from UE to eNodeB. Function returns -1 on error and 0 on success
+int send_rrc_setup_complete(int socketfd, s_message* message) {
+    message->message_type = rrc_complete;
+    message->message_value.message_complete.mcc = 234;
+    message->message_value.message_complete.mnc = 15;
 
     if(-1 == write(socketfd, (s_message*)message, sizeof(*message)))
         return -1;
--- clientLTE/rrc.h
+++ clientLTE/rrc.h
@@ -12,5 +12,6 @@
 
 void generate_ue_identity(s_stmsi*);
 int send_rrc_connection_request(int, s_message*, void (*generate_ue_identity_func)(s_stmsi*));
+int send_rrc_setup_complete(int socketfd, s_message* message);
 
 #endif
