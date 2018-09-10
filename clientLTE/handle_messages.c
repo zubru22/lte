2e32d1973046d7f178967611b0899a1293643aa1
Ryszard.Sinius <ryszard.sinius@tieto.com>
2018-09-10 02:16:29 -0700

    Functions to receive and send ping added

--- clientLTE/handle_messages.c
+++ clientLTE/handle_messages.c
@@ -1,10 +1,11 @@
 /* This functionality is about handling default client server communication like pings and stuff */
+#include "handle_messages.h"
 
 // This function receives ping message from eNodeB. Function returns -1 on error, 0 on success.
 int receive_ping(int socketfd, s_message* message) {
-    if(-1 == read(socketfd, (s_message*)message, sizeof(*message))
+    if(-1 == read(socketfd, (s_message*)message, sizeof(*message)))
         return -1;
-        
+
     return 0;
 }
 
