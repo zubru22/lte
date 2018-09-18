e3d96c7feb6df7bfb43931a17a0e136822c79906
Ryszard.Sinius <ryszard.sinius@tieto.com>
2018-09-18 04:28:02 -0700

    Make downloading of a file from eNodeBs and receiving PINGs working on other threads.
    https://trello.com/c/1rudvd9t/16-make-downloading-of-a-file-from-enodebs-and-receiving-pings-working-on-other-threads

--- clientLTE/main.c
+++ clientLTE/main.c
@@ -19,11 +19,30 @@
 #ifndef SHUTDOWN_H
 #include "shutdown.h"
 #endif
+#ifndef PTHREAD_H
+#include <pthread.h>
+#endif
 
 volatile bool running = true;
+int socket_fd;
+s_message message;
+ue_battery battery;
 
 void signal_handler(int signum) {
     running = false;
+}
+
+void* ping_thread() {
+    while(running) {
+        if (receive_ping(socket_fd, &message) == 0) {
+            if (send_pong(socket_fd, &message) == -1)
+                add_logf(client_log_filename, LOG_ERROR, "Failed to response to server ping!");
+            else {
+                decrease_after_ping(socket_fd, &message, &battery);
+                add_logf(client_log_filename, LOG_SUCCESS, "Successfully handled server ping!");
+            }
+        }
+    }
 }
 
 int main(int argc, char* argv[])
@@ -33,18 +52,20 @@ int main(int argc, char* argv[])
         return 0;
     }
     int port_number = atoi(argv[1]);
-    int socket_fd;
     struct sockaddr_in server;
-    s_message message;
-    ue_battery battery;
     s_cells cells;
     FILE* file_to_recv;
+    pthread_t ping_thread_id;
+
     srand(time(NULL)); 
 
     struct sigaction s_signal;
     s_signal.sa_handler = signal_handler;
     sigemptyset(&s_signal.sa_mask);
     s_signal.sa_flags = 0;
+
+
+    // INITIALIZATION BLOCK /////////////////////////////////////
 
     initialize_battery_life(&battery);
     initialize_cells(&cells);
@@ -57,6 +78,9 @@ int main(int argc, char* argv[])
         add_logf(client_log_filename, LOG_ERROR, "Failed to connect!");
         return 0;
     }
+
+    /////////////////////////////////////////////////////////////
+
     //returns -1 on error, else 0
     if (send_prach_preamble(socket_fd, &message, generate_ra_rnti) == -1) {
         add_logf(client_log_filename, LOG_ERROR, "Failed to send preamble!");
@@ -102,21 +126,18 @@ int main(int argc, char* argv[])
     receive_rrc_setup(socket_fd, &received, &message);
     sigaction(SIGINT, &s_signal, NULL);
 
+    // Create another thread for pinging
+    if(pthread_create(&ping_thread_id, NULL, ping_thread, NULL) != 0) {
+        add_logf(client_log_filename, LOG_ERROR, "Failed to create a thread: ping_thread_id");
+        exit(1);
+    }
+
     // While running and have not received eNodeB shutdown message
     while (running && !check_for_shutdown(socket_fd, &received)) {
         update_battery(socket_fd, &message, &battery);
         set_current_signal_event(&cells);
         printf("\nCurrent event: %d\n", (int)cells.current_event+1);
         printf("Battery power: %i\n", battery.power_percentage);
-
-        if (receive_ping(socket_fd, &message) == 0) {
-            if (send_pong(socket_fd, &message) == -1)
-                add_logf(client_log_filename, LOG_ERROR, "Failed to response to server ping!");   
-            else {
-                decrease_after_ping(socket_fd, &message, &battery);
-                add_logf(client_log_filename, LOG_SUCCESS, "Successfully handled server ping!");
-            }
-        }
 
         if (receive_measurement_control_request(socket_fd, &received))
             send_measurement_report(socket_fd, &message, &cells);
@@ -126,7 +147,11 @@ int main(int argc, char* argv[])
 
         sleep(1);
     }
-
+    // Join thread
+    if(pthread_join(ping_thread_id, NULL) != 0) {
+        add_logf(client_log_filename, LOG_ERROR, "Failed to join a thread: ping_thread_id");
+        exit(1);
+    }
     // Check if eNodeB is still on before trying to send UE off signal
     if (message.message_type != enb_off)
     {
